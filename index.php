d9669132fe45b5610f217748cf701a0c88074991
hossamzee <hossam_zee@yahoo.com>
2014-09-01 20:05:35 +0300

    Done with index.php file.

--- index.php
+++ index.php
@@ -86,8 +86,14 @@ else
 	}
 
 	// Get the past events.
-	$get_past_events_query = mysql_query("SELECT * FROM event WHERE (day + month*29 + year*355) < $now_date_int ORDER BY id DESC LIMIT $events_limit");
-	$past_events_count = mysql_num_rows($get_past_events_query);
+	$get_past_events_query = $dbh->prepare("SELECT * FROM event WHERE (day + month*29 + year*355) < :now_date_int ORDER BY id DESC LIMIT :events_limit");
+
+	$get_past_events_query->bindParam(":now_date_int", $now_date_int);
+	$get_past_events_query->bindParam(":events_limit", $events_limit);
+	
+	$get_past_events_query->execute();
+
+	$past_events_count = $get_past_events_query->rowCount();
 		
 	if ($past_events_count == 0)
 	{
@@ -97,7 +103,7 @@ else
 	{
 		$past_events = "<ul>";
 		
-		while ($past_event = mysql_fetch_array($get_past_events_query))
+		while ($past_event = $get_past_events_query->fetch(PDO::FETCH_ASSOC))
 		{
 			$past_events .= "<li><a href='calendar.php?action=view_event&id=$past_event[id]' class='whitelink'>$past_event[title]</a> <span class='datetime'>(في $past_event[day]/$past_event[month]/$past_event[year])</span></li>";
 		}
@@ -115,51 +121,7 @@ else
 		)
 	);
 	
-	//$family_council = "<li><a href='family_council.php' class='main_family_council'>مجلس العائلة <i class='icon-chevron-down icon-white'></i></a><ul class='popup_menu'><li><a href='secretariat.php'>أمانة المجلس</a></li> <li><a href='family_dean.php'>عميد العائلة</a></li> <li><a href='regulations.php'>اللائحة التظيمية</a></li> <li><a href='committees.php'>اللجان</a></li></ul></li>";
-	// TODO: Uncomment when committees have been released. $congratulations = get_committees_nominee_congratulations();
 	$congratulations = "";
-	
-	/*
-	if ($current_hijri_month == 8)
-	{
-		// Get the not answered question.
-		$get_ramadan_question_query = mysql_query("SELECT * FROM ramadan_question WHERE id NOT IN (SELECT question_id FROM member_question WHERE member_id = '$user[member_id]') AND (day <= $current_hijri_day) ORDER BY day DESC");
-	
-		if (mysql_num_rows($get_ramadan_question_query) > 0)
-		{
-			$fetch_question = mysql_fetch_array($get_ramadan_question_query);
-			$answers = array();
-	
-			// Get the answers.		
-			for ($i=1; $i<=4; $i++)
-			{
-				if (!empty($fetch_question["answer$i"]))
-				{
-					$answers[$i] = $fetch_question["answer$i"];
-				}
-			}
-		
-			$answers_html = "";
-		
-			// Walk up-on the answers.
-			foreach ($answers as $key => $value)
-			{
-				$answers_html .= "<input type='radio' name='answer' value='$key' id='answer$key' /> $value<br />";
-			}
-	
-			// Get Ramadan question(s).
-			$ramadan_question = template(
-				"views/ramadan_question.html",
-				array(
-					"question_id" => $fetch_question["id"],
-					"question" => $fetch_question["question"],
-					"answers" => $answers_html,
-					"positive_message" => $fetch_question["positive_message"],
-				)
-			);
-		}
-	}
-	*/
 }
 
 $ramadan_question = "";
@@ -173,7 +135,6 @@ $content = template(
 		"logged" => $logged,
 		"inside" => $inside,
 		"ramadan_question" => $ramadan_question,
-		// TODO: Uncomment when committees have been released. "family_council" => $family_council,
 		"version" => version
 	)
 );
