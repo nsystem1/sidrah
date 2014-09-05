5610a63ffe029fa55a837451187a1c4c4350cae8
World Wide Web Server <_www@Hussams-MacBook-Pro.local>
2014-09-06 01:48:43 +0300

    Replace mysql_fetch_array calls with PDO::fetch in fix_media_comments.php file.

--- fix_media_comments.php
+++ fix_media_comments.php
@@ -15,7 +15,7 @@ while ($media = mysql_fetch_array($get_medias_query))
 $dbh->bindParam(":media_id", $media["id"]);
 $dbh->execute();
 
-	$fetch_media_comments = mysql_fetch_array($get_media_comments_query);
+	$fetch_media_comments = $get_media_comments_query->fetch(PDO::FETCH_ASSOC);
 	
 	draw_comments_count_thumb($media["name"], $fetch_media_comments["comments_count"]);
 	echo "$media[id]<br />";
