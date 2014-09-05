7bb06a663c43c69413ec84c0c5a5191534cb2842
World Wide Web Server <_www@Hussams-MacBook-Pro.local>
2014-09-06 00:46:45 +0300

    Removing mysql_real_escape_string calls from init_send_user_info.php file.

--- init_send_user_info.php
+++ init_send_user_info.php
@@ -3,7 +3,7 @@
 require_once("inc/functions.inc.php");
 
 $user = user_information();
-$member_id = mysql_real_escape_string(@$_GET["member_id"]);
+$member_id = @$_GET["member_id"];
 
 // Check if the member does exist.
 $member = get_member_id($member_id);
