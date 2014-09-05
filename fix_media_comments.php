b9744c28624702aee957ece692f0848cf0275c51
World Wide Web Server <_www@Hussams-MacBook-Pro.local>
2014-09-06 01:21:58 +0300

    Replace mysql_query calls with PDO::prepare in fix_media_comments.php file.

--- fix_media_comments.php
+++ fix_media_comments.php
@@ -4,12 +4,17 @@
 require_once("inc/functions.inc.php");
 
 // Get all medias.
-$get_medias_query = mysql_query("SELECT * FROM media");
+$get_medias_query = $dbh->prepare("SELECT * FROM media");
+$dbh->execute();
+
 
 while ($media = mysql_fetch_array($get_medias_query))
 {
 	// Get the count of media comments.
-	$get_media_comments_query = mysql_query("SELECT COUNT(id) AS comments_count FROM media_comment WHERE media_id = '$media[id]'");
+	$get_media_comments_query = $dbh->prepare("SELECT COUNT(id) AS comments_count FROM media_comment WHERE media_id = :media_id");
+$dbh->bindParam(":media_id", $media["id"]);
+$dbh->execute();
+
 	$fetch_media_comments = mysql_fetch_array($get_media_comments_query);
 	
 	draw_comments_count_thumb($media["name"], $fetch_media_comments["comments_count"]);
