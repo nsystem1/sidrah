bb17d9b164ec99b460b120e5baeac492e1cd72c6
World Wide Web Server <_www@Hussams-MacBook-Pro.local>
2014-09-17 21:20:16 +0300

    Replace mysql_fetch_array calls with PDO::fetch in mobile_book.php file.

--- mobile_book.php
+++ mobile_book.php
@@ -27,7 +27,7 @@ if ($get_locations_query->rowCount() == 0)
 $mobile_book_html = "";
 $i = 1;
 
-while ($location = mysql_fetch_array($get_locations_query))
+while ($location = $get_locations_query->fetch(PDO::FETCH_ASSOC))
 {
 	$string_location = empty($location["location"]) ? "(غير محدّد)" : $location["location"];
 	$mobile_book_html .= "<h3>$string_location <span>($location[members_count])</span></h3>\n";
@@ -36,7 +36,7 @@ while ($location = mysql_fetch_array($get_locations_query))
 	// Get all members in the current location.
 	$get_current_members_query = mysql_query("SELECT id, fullname, mobile, company_id, job_title, privacy_company, privacy_job_title FROM member WHERE location = '$location[location]' AND privacy_mobile != 'related_circle' AND is_alive = 1 AND gender = 1 AND mobile LIKE '5%' AND tribe_id = '$main_tribe_id' ORDER BY fullname");
 	
-	while ($one_member = mysql_fetch_array($get_current_members_query))
+	while ($one_member = $get_current_members_query->fetch(PDO::FETCH_ASSOC))
 	{
 		// Get the company of the current member.
 		$company = ($one_member["privacy_company"] != "related_circle") ? rep_company($one_member["company_id"]) : "";
