ca2ba28fcc9bdea8242d783f86c97657f21473fb
hossamzee <hossam_zee@yahoo.com>
2014-09-19 00:06:46 +0300

    Modifying config.inc.default.php file.

--- inc/config.inc.default.php
+++ inc/config.inc.default.php
@@ -6,36 +6,41 @@ define("database_username", "root");
 define("database_password", "");
 define("database_name", "sidrah");
 
-// Tribe
-define("main_tribe_id", 1);
-
-// Salt
-define("sidrah_salt", "Co4a354syisHBpdAODIESFWsXXOeG4YQ76OiSO6gi21kJq3AbDqRxLkFgc2O8iC");
-
-// Url
-define("script_url", "http://localhost/sidrah");
-
-// Upload
-define("media_max_size", 6144); // KBytes
-define("media_large_width", 640);
-define("media_large_height", 640);
-define("media_thumb_width", 64);
-define("media_thumb_height", 64);
-
 // SMS
 define("sms_username", "");
 define("sms_password", "");
 define("sms_sender", "");
+define("sms_delete_key", "");
+
+// Tribe
+define("main_tribe_name", "");
+define("main_tribe_id", "");
+
+// Salt
+define("sidrah_salt", "");
+
+// Url
+define("script_url", "");
 
 // User
-define("user_min_length", 4);
-define("user_max_length", 40);
+define("user_min_length", "4");
+define("user_max_length", "40");
+
+// Member
+define("member_min_names", "4");
+
+// Stage
+define("sidrah_stage", "");
+
+// Upload
+define("media_max_size", "6144");
+define("media_large_width", "640");
+define("media_large_height", "640");
+define("media_thumb_width", "64");
+define("media_thumb_height", "64");
 
 // Version
-define("version", "1.0.0");
+define("version", "1.1");
 
-// Marital Statuses
-// single = 1
-// married = 2
-// divorced = 3
-// widow/widower = 4;
+// Author
+define("sidrah_author", "حسام الزغيبي");
