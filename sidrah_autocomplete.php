251f46b074cd270fd1f7b817c92cb2dd1dac22d6
hossamzee <hossam_zee@yahoo.com>
2014-09-20 15:33:24 +0300

    Fix auto completing members by fixing PDO calls.

--- sidrah_autocomplete.php
+++ sidrah_autocomplete.php
@@ -22,11 +22,11 @@ $return = @$_GET["return"];
 // Type.
 // mother, father, wife, husband.
 
-$firstname = "";
+$first_name = "";
 $error = false;
 $exact_array = array();
 $almost_exact_array = array();
-$childof_array = array();
+$child_of_array = array();
 $suggested_array = array();
 $normalized_name = normalize_name($name);
 
@@ -44,69 +44,9 @@ else
 {
 	$main_tribe_id = main_tribe_id;
 	$names = explode(" ", $normalized_name);
-	$firstname = $names[0];
+	$first_name = $names[0];
 
-	// If it was for family dean.
-	if ($type == "dean-auto-complete")
-	{
-		$href = "#autocomplete-names-$unique_id";
-		$search_for = escape_confusing("^" . implode(" ", $names) . ".*$");
-		$get_related_names_query = $dbh->prepare("SELECT id, fullname, photo, age, location, job_title FROM member WHERE fullname REGEXP :search_for AND tribe_id = :main_tribe_id AND is_alive = 1 AND gender = '1' AND age >= 45 ORDER BY fullname ASC LIMIT 8");
-        $get_related_names_query->bindParam(":search_for", $search_for);
-        $get_related_names_query->bindParam(":main_tribe_id", $main_tribe_id);
-        $get_related_names_query->execute();
-		
-		if ($get_related_names_query->rowCount() > 0)
-		{
-			echo "<ul class='ul_result'>";
-			
-			while ($result = $get_related_names_query->fetch(PDO::FETCH_ASSOC))
-			{	
-				$photo = rep_photo($result["photo"], 1, "avatar");
-				$detailed_info = "$result[age] سنة - $result[location] - $result[job_title]";
-				
-				echo "<li class='li_result'><a title='$result[fullname]' href='$href' data-id='$result[id]' class='result'>$photo $result[fullname]<div class='dean_detailed'>$detailed_info</div><div class='clear'></div></a></li>";
-			}
-			
-			echo "</ul>";
-		}
-		else
-		{
-			echo "<div class='error'><i class='icon-exclamation-sign'></i> لا يوجد نتائج.</div>";
-		}
-		
-		return;
-	}
-	
-	// If it was for box account.
-	if ($type == "accounts-auto-complete")
-	{
-		$href = "#autocomplete-names-$unique_id";
-		$search_for = escape_confusing("^" . implode(" ", $names) . ".*$");
-		$get_related_names_query = mysql_query("SELECT box_account.id AS id, box_account.iban AS iban, member.fullname AS fullname FROM box_account, member, box_subscriber WHERE box_account.subscriber_id = box_subscriber.id AND box_subscriber.member_id = member.id AND member.tribe_id = '$main_tribe_id' AND member.is_alive = 1 AND member.gender = '1' AND (fullname REGEXP '$search_for' OR iban REGEXP '$search_for') ORDER BY fullname ASC LIMIT 8")or die();
-		
-		if ($get_related_names_query->rowCount() > 0)
-		{
-			echo "<ul class='ul_result'>";
-			
-			while ($result = $get_related_names_query->fetch(PDO::FETCH_ASSOC))
-			{	
-				$detailed_info = "$result[iban]";
-				
-				echo "<li class='li_result'><a title='$result[fullname]' href='$href' data-id='$result[id]' class='result'>$result[fullname]<div>$detailed_info</div><div class='clear'></div></a></li>";
-			}
-			
-			echo "</ul>";
-		}
-		else
-		{
-			echo "<div class='error'><i class='icon-exclamation-sign'></i> لا يوجد نتائج.</div>";
-		}
-		
-		return;
-	}
-
-	// If it was just autocomplete.
+	// If it was just auto complete.
 	if ($type == "auto-complete")
 	{
 		$condition = "";
@@ -114,14 +54,14 @@ else
 		if ($user["group"] == "visitor" || $user["group"] == "user")
 		{
 			$main_tribe_id = main_tribe_id;
-			$condition = "AND gender = '1' AND tribe_id = '$main_tribe_id'";
+			$condition = "AND gender = 1 AND tribe_id = '$main_tribe_id'";
 		}
 
 		$href = "#autocomplete-names-$unique_id";
 		$search_for = escape_confusing("^" . implode(" ", $names) . ".*$");
-		$get_related_names_query = $dbh->prepare("SELECT id, fullname FROM member WHERE fullname REGEXP :search_for :condition ORDER BY fullname ASC LIMIT 8");
-        $get_related_names_query->bindParam(":search_for", $search_for);
-        $get_related_names_query->bindParam(":condition", $condition);
+		$get_related_names_query = $dbh->prepare("SELECT id, fullname FROM member WHERE fullname REGEXP :search_for $condition ORDER BY fullname ASC LIMIT 8");
+        $get_related_names_query->bindValue(":search_for", $search_for, PDO::PARAM_STR);
+        //$get_related_names_query->bindParam(":condition", $condition);
         $get_related_names_query->execute();
 
 		if ($get_related_names_query->rowCount() > 0)
@@ -211,11 +151,11 @@ else
 		{
 			while ($childof = $get_childof_query->fetch(PDO::FETCH_ASSOC))
 			{
-				$iname = "$firstname $childof[fullname]";
+				$iname = "$first_name $childof[fullname]";
 				
 				if (!in_array($iname, $exact_array) && !in_array($iname, $almost_exact_array))
 				{
-					$childof_array []= $childof["fullname"];
+					$child_of_array []= $childof["fullname"];
 				}
 			}
 		}
@@ -248,19 +188,19 @@ if (count($almost_exact_array) > 0)
 	echo "</ul>";
 }
 
-if (count($childof_array) > 0)
+if (count($child_of_array) > 0)
 {
-	echo "<div class='results_group'>إضافة <b>$firstname</b> إلى</div><ul class='ul_result'>";
+	echo "<div class='results_group'>إضافة <b>$first_name</b> إلى</div><ul class='ul_result'>";
 	
-	for($i=0; $i<count($childof_array); $i++)
+	for($i=0; $i<count($child_of_array); $i++)
 	{
-		echo "<li class='li_result'><a title='$firstname $childof_array[$i]' href='$href' class='result'>$childof_array[$i]</a></li>";
+		echo "<li class='li_result'><a title='$first_name $child_of_array[$i]' href='$href' class='result'>$child_of_array[$i]</a></li>";
 	}
 	
 	echo "</ul>";
 }
 
-if ((count($exact_array) == 0) && (count($almost_exact_array) == 0) && (count($childof_array) == 0) && $error == false)
+if ((count($exact_array) == 0) && (count($almost_exact_array) == 0) && (count($child_of_array) == 0) && $error == false)
 {
 	echo "<div class='results_group'>إضافة اسم جديد</div>";
 	echo "<ul class='ul_result'><li class='li_result'><a title='$normalized_name' href='$href' class='result'>$normalized_name</a></li></ul>";
